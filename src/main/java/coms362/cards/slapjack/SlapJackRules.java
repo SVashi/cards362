f39d6843d23332cc02dd9451c1e227b9ae27bec9
Andrew pester <apester@iastate.edu>
2021-11-27 14:52:04 -0600

    rules and both moves seem to be working well

--- src/main/java/coms362/cards/slapjack/SlapJackRules.java
+++ src/main/java/coms362/cards/slapjack/SlapJackRules.java
@@ -87,42 +87,29 @@ public class SlapJackRules extends RulesDispatchBase implements Rules, RulesDisp
 
 	public Move apply(CardEvent e, Table table, Player player)
 	{	
-//		Pile p1Pile = table.getPile(PLAYER_ONE_PILE);
-//		Pile p2Pile = table.getPile(PLAYER_TWO_PILE);
-//		Pile toPile = table.getPile(DISCARD_PILE);
-//		Card c;
+		Pile playerPile = table.getPile(""+player.getPlayerNum());
+		Pile toPile = table.getPile(DISCARD_PILE);
+		Card c;
 //		//always check if they chose from the center pile and if so check the rank of the card
-//		c = toPile.getCard(e.getId());
-//		if(c != null && c.getRank() == 11){
+		c = toPile.getCard(e.getId());
+		if(c != null && c.getRank() == 11){
 //			//if the rank is 11 it is a jack so that player will win the round
-//			return new SlapJackRoundWinMove(player, toPile , player.getPlayerNum() == 1 ? p1Pile : p2Pile);
-//		}
-//		//just does a flipflop between player 1 and 2 as there is only 2 players
-//		// does this as nothing else is implemented
-//		if(turn == 1 && player.getPlayerNum() == 1){
-//			//yes this is repeated code for each player just a quick mockup to see how things worked
-//			//this is when it is player 1's turn and they are the person who selected a card currently
-//			c = p1Pile.getCard(e.getId());
-//			if (c == null) {
-//				//this checks if they selected from their deck
-//				return new DropEventCmd();
-//			}
-//			//flip turn after it is determined that they selected from their pile
-//			turn = 2;
-//			return new SlapJackMove(c, player, p1Pile, toPile);
-//		}else if(turn == 2 && player.getPlayerNum() == 2){
-//			//this is when it is player 2's turn and they are the person who selected a card currently
-//			c = p2Pile.getCard(e.getId());
-//			if (c == null) {
-//				return new DropEventCmd();
-//			}
-//			turn = 1;
-//			return new SlapJackMove(c, player, p2Pile, toPile);
-//		}else{
-//			//if it is not their turn or they selected from the wrong deck then 
-//			return new DropEventCmd();
-//		}	
-		return null;
+			// TODO
+			return new SlapJackRoundWinMove(player, toPile , playerPile);
+		}
+		if(turn == player.getPlayerNum()){
+			c = playerPile.getCard(e.getId());
+			if (c == null) {
+				return new DropEventCmd();
+			}
+			//turn doesn't work
+			
+			turn %= table.getPlayers().size();
+			turn++;
+			return new SlapJackMove(c, player, playerPile, toPile);
+		}else{
+			return new DropEventCmd();
+		}
 	}
 	
 	private void registerEvents()
